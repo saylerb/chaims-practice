cf65732b4b26b0390efc3bb1a09c410830c96641
Brian Sayler <sayler.b@gmail.com>
2016-07-24 21:46:18 -0600

    add simple artist service and correct spec

--- spec/services/artist_service_spec.rb
+++ spec/services/artist_service_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 describe ArtistService do
   it "returns a list of Artists" do
     VCR.use_cassette("artists") do
-      artists = ArtistService.new.artists(nil)
+      artists = ArtistService.new.artists
       artist = artists.first
 
+      expect(artist[:id]).to eq(3)
       expect(artist[:name]).to eq("Kip Moore")
     end
   end
