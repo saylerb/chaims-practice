6ea3371f55a706f3775acaa53c17d23e8bb75863
Brian Sayler <sayler.b@gmail.com>
2016-07-24 22:11:50 -0600

    add artist show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "artists#index"
+  get '/artist/:id', to: "artists#show", as: :artist_show
 end
