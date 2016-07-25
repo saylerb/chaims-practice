908d2eb4dcfae3b36974416bea71b09a57a68b3a
Brian Sayler <sayler.b@gmail.com>
2016-07-24 22:00:30 -0600

    add artist openstruct model

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,14 @@
-class Artist < ApplicationRecord
+class Artist < OpenStruct
+
+  def self.service
+    @@service ||= ArtistService.new
+  end
+
+  def self.all
+    all_artists_data = service.get_artists
+
+    all_artists_data.map do |artist_data|
+      Artist.new(artist_data)
+    end
+  end
 end
